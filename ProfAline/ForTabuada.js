02660e755c29d0885054d2cd23e9f9e6cdf945c9
GUSTAVO OLIVEIRA <gugutoktpi2@gmail.com>
2025-04-07 08:22:11 -0300

    Arquivos utilizados ou alterados na aula

--- ProfAline/ForTabuada.js
+++ ProfAline/ForTabuada.js
@@ -1,22 +1,8 @@
-var conceito;
-var nota1 = 1;  
-var nota2 = 1; 
-var media = (nota1 + nota2) / 2;
+var tabuada=2;
 
-var conceito;
+for(var i=0; i<=10;i++){
 
-if (media >= 9.1 && media <= 10.0) {
-    conceito = "A";
-} else if (media >= 7.6 && media < 9.1) {
-    conceito = "B";
-} else if (media >= 6.1 && media < 7.6) {
-    conceito = "C";
-} else if (media >= 4.1 && media < 6.1) {
-    conceito = "D";
-} else {
-    conceito = "E";
+console.log(tabuada, "X", i, "=",tabuada *i);
+
 }
 
-var situacao = (conceito === "A" || conceito === "B" || conceito === "C") ? "APROVADO" : "REPROVADO";
-
-console.log("Notas: " + nota1 + " e " + nota2 + "\nMédia: " + media.toFixed(2) + "\nConceito: " + conceito + "\n" + situacao);
